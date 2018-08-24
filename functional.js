183f571c5808d7bb70c9389c519c142896d2b6b4
Romenig Lima Damasio <romenigld@gmail.com>
2018-08-24 23:11:56 +0200

    6.1.1) Separate auxiliary function called urlify and the Exercise 1.

--- functional.js
+++ functional.js
@@ -1,10 +1,16 @@
 let states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"];
 
+// Returns a URL-friendly version of a string.
+//   Example: "North Dakota" -> "north-dakota"
+function urlify(string) {
+  return string.toLowerCase().split(/\s+/).join("-");
+}
+
 // urls: Imperative version
 function imperativeUrls(elements) {
   let urls = [];
   elements.forEach(function(element) {
-    urls.push(element.toLowerCase().split(/\s+/).join("-"));
+    urls.push(urlify(element));
   });
   return urls;
 }
@@ -12,6 +18,12 @@ console.log(imperativeUrls(states));
 
 // urls:: Functional version
 function functionalUrls(elements) {
-  return states.map(elements => elements.toLowerCase().split(/\s+/).join('-'));
+  return elements.map(element => urlify(element));
 }
 console.log(functionalUrls(states));
+
+//Exercise 1) urls:: Functional Full URLs version
+function functionalFullUrls(elements) {
+  return elements.map(element => `https://example.com/${urlify(element)}`);
+}
+console.log(functionalFullUrls(states));
