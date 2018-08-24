22242fa09511c8b388c39356130970c927809093
Romenig Lima Damasio <romenigld@gmail.com>
2018-08-24 02:09:15 +0200

    5.1.1) Functions definitions - exercises

--- sorting_numerical_arrays.js
+++ sorting_numerical_arrays.js
@@ -14,3 +14,15 @@ function numberCompare(a, b) {
   console.log(`The sort of the array 'a' alphabatically is: ${a.sort()}`);
 
   console.log(`Sorting with the function numberCompare is : ${a.sort(numberCompare)}`);
+
+function square (x) {
+  return x * x;
+}
+
+console.log("The function square of 2 is: " + square(2));
+
+let altSquare = (y) => {
+  return y * y;
+}
+
+console.log("The function altSquare with fat arrow of 3 is: " + altSquare(3));
