1b6a11b966e2c70ecfe842e91603bf76f5e1d50f
Romenig Lima Damasio <romenigld@gmail.com>
2018-08-25 14:48:51 +0200

    7.1.1) Defining Objects - Exercise 1

--- palindrome.js
+++ palindrome.js
@@ -7,6 +7,10 @@ function reverse(string) {
 function Phrase(content) {
   this.content = content;
 
+  // Makes the phrase LOUDER.
+  this.louder = function() {
+    return this.content.toUpperCase();
+  }
   // Returns true if the phrase is a palindorme, false otherwise.
   this.palindrome = function palindrome() {
     let processedContent = this.content.toLowerCase();
