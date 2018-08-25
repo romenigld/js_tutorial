1d44c33c2b4df53e8fb0ddcd69872b077c54dc5b
Romenig Lima Damasio <romenigld@gmail.com>
2018-08-25 14:45:24 +0200

    7.1) Defining Objects

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,15 @@
-// Function Palindorme
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
-}
-
 // Reverses a String
 function reverse(string) {
   return Array.from(string).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns true if the phrase is a palindorme, false otherwise.
+  this.palindrome = function palindrome() {
+    let processedContent = this.content.toLowerCase();
+    return processedContent === reverse(processedContent);
+  }
 }
