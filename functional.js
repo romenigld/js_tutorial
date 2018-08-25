b42bcd42462825243ea7ba960a0f31522e225423
Romenig Lima Damasio <romenigld@gmail.com>
2018-08-25 02:19:03 +0200

    6.3) Reduce, example 2

--- functional.js
+++ functional.js
@@ -76,3 +76,22 @@ function functionalSum(elements) {
   return elements.reduce((total, n) => { return total += n; });
 }
 console.log(functionalSum(numbers));
+
+// lengths: Imperative solution
+function imperativeLengths(elements) {
+  let lengths = {};
+  elements.forEach(function(element){
+    lengths[element] = element.length;
+  });
+  return lengths;
+}
+console.log(imperativeLengths(states));
+
+// lengths: Functional solution
+function functionalLengths(elements){
+  return elements.reduce((lengths, element) => {
+                          lengths[element] = element.length;
+                          return lengths;
+                        }, {});
+}
+console.log(functionalLengths(states));
