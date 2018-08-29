359f8ced1189227e1ea2aa8f43873f84db877e11
Romenig Lima Damasio <romenigld@gmail.com>
2018-08-29 22:22:05 +0200

    9.3) Dynamic HTML

--- bundle.js
+++ bundle.js
@@ -5,11 +5,12 @@ let Phrase = require("romenigld-palindrome");
 function palindromeTester() {
   let string = prompt("Please enter a string for a palindrome testing:");
   let phrase = new Phrase(string);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
   if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
   } else {
-    alert(`"${phrase.content}" is not a palindrome.`);
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
   }
 }
 
