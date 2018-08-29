95a69f15292d2665375a177b889e46eb1a12f320
Romenig Lima Damasio <romenigld@gmail.com>
2018-08-29 21:37:20 +0200

    9.2) Event listeners

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 let Phrase = require("romenigld-palindrome");
 //alert(new Phrase("Madam, I'm Adam.").palindrome());
-let string = prompt("Please enter a string for a palindrome testing:");
-let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`);
+function palindromeTester() {
+  let string = prompt("Please enter a string for a palindrome testing:");
+  let phrase = new Phrase(string);
+
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome!`);
+  } else {
+    alert(`"${phrase.content}" is not a palindrome.`);
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("click", function() {
+    palindromeTester();
+  });
+});
